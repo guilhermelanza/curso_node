4dcfa5ac82ae12d5a0f15577f39c5d3306d2053e
Guilherme Canavarollo Lanza da Silva | S2IT <guilherme.lanza@S2IT.CORP>
2019-01-04 17:06:27 -0200

    form inclusao

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,10 +1,11 @@
-//var dbConnection = require('../../config/dbConnection.js');
+module.exports = function(application) {
 
-module.exports = function(app) {
+	application.get('/noticias', function(req, res){		
+		
+		var connection = application.config.dbConnection();
+		var noticiasModel = application.app.models.noticiasModel;
 
-	app.get('/noticias', function(req, res){		
-		var connection = app.config.dbConnection();
-		connection.query('select * from noticias', function(error, result){
+		noticiasModel.getNoticias(connection, function(error, result){
 			res.render("noticias/noticias", {noticias : result});
 		});
 	});	
